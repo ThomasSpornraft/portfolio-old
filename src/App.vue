aff57ced220b2708a06ef053c5cb21b8fc3000f2
Thomas Spornraft <Thomas.Spornraft@web.de>
2021-10-11 12:59:25 +0200

    --

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header></Header>
+
   <router-view />
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: { Header },
+};
+</script>
+
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <div class="header__logo">
+      <div class="header__logo-img">
+        <img src="../assets/profil.png" alt="" />
+      </div>
+    </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/ausbildung">Ausbildung</router-link>
+      <router-link to="/erfahrung">Erfahrung</router-link>
+      <router-link to="/skills">Skills</router-link>
+      <router-link to="/sonstiges">Sonstiges</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
