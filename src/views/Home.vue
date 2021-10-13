c623f48ced2e003fa0f0fc43c57477c8f081f965
Thomas Spornraft <thomas.spornraft@web.de>
2021-10-14 01:43:48 +0200

    --

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -185,6 +185,7 @@ body {
 }
 
 .home__content-contact img {
+  margin-top: 10px;
   border: 5px solid lightseagreen;
   padding: 5px;
   width: 200px;
@@ -226,9 +227,7 @@ body {
 
 .home__content-contact-list ul li i {
   font-size: 32px;
-
   cursor: pointer;
-
   padding: 7px;
 }
 
